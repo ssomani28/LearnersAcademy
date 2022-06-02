7fae4f828a78fb6539e7240b177f060cc00b3aa8
ssomani28 <102278536+ssomani28@users.noreply.github.com>
2022-06-02 22:35:15 +0530

    Add files via upload

---
+++ LearnersAcadamy/src/main/java/com/siddhant/DB_CONNECTION.java
@@ -0,0 +1,19 @@
+package com.prajval;
+	import java.sql.Connection;
+	import java.sql.DriverManager;
+	import java.sql.SQLException;
+
+public class DB_CONNECTION {
+
+	final static String DB_URL = "jdbc:mysql://localhost:3306/student";
+		final static String USER_NAME ="root";
+		final static String USER_PASS = "Armed@123$";
+		final static String DRIVER = "com.mysql.jdbc.Driver";
+		
+			public static Connection getConn() throws ClassNotFoundException, SQLException
+			{
+				Class.forName(DRIVER);
+				Connection con = DriverManager.getConnection(DB_URL,USER_NAME,USER_PASS);
+				return con;
+			}
+	}
